58d5f4dafc5ad4a421d628e372ceec232cd72ec3
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-11-02 01:10:34 -0400

    iterating through the ballots, then itterate through each row

--- ex1.rb
+++ ex1.rb
@@ -1,3 +1,11 @@
+# Each hash represents a voting ballot, with three names in 1st, 2nd, and 3rd place. A first place vote is worth 3 points, a 2nd place vote is worth 2 points, and a 3rd place vote is worth 1 point.
+#
+# For example, the first ballot {1 => 'Smudge', 2 => 'Tigger', 3 => 'Simba'} shows that this voter chose Smudge for first place, Tigger for 2nd, and Simba for 3rd. Smudge would be awarded 3 points, Tigger would be awarded 2 points, and Simba would be awarded 1 point.
+#
+# Tally up all the votes and determine who won.
+
+
+
 ballots = [{ 1 => 'Smudge', 2 => 'Tigger', 3 => 'Simba' },
            { 1 => 'Bella', 2 => 'Lucky', 3 => 'Tigger' },
            { 1 => 'Bella', 2 => 'Boots', 3 => 'Smudge' },
@@ -5,3 +13,21 @@ ballots = [{ 1 => 'Smudge', 2 => 'Tigger', 3 => 'Simba' },
            { 1 => 'Lucky', 2 => 'Felix', 3 => 'Bella' },
            { 1 => 'Smudge', 2 => 'Simba', 3 => 'Felix' }]
 
+
+
+first_place =
+
+# rows = ballots.each.with_index do |row, index|
+#   row.each.with_index do |value, index|
+#     p value
+#   end
+#
+# end
+
+
+ballots.each do |row|
+  # puts row
+  row.each do |key, value|
+    puts "Key #{key}, Value: #{value}"
+  end
+end
